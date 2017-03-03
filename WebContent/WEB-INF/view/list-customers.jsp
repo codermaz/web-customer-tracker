a554f52a5f8c24f59f41fd57d53fbb2da443c7a8
codermaz <a@MacMinis-Mac-mini.local>
2017-03-03 19:40:28 +0100

    Search button done

--- WebContent/WEB-INF/view/list-customers.jsp
+++ WebContent/WEB-INF/view/list-customers.jsp
@@ -1,43 +1,69 @@
 
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title> Customers list</title>
-	<link type="text/css"
-		  rel="stylesheet"
-		  href="${pageContext.request.contextPath}/resources/css/style.css" />
+<title>Customers list</title>
+<link type="text/css" rel="stylesheet"
+	href="${pageContext.request.contextPath}/resources/css/style.css" />
 </head>
 
 <body>
 
 	<div id="wrapper">
 		<div id="header">
-			<h2> CRM - Customer Relationship Manager </h2>
+			<h2>CRM - Customer Relationship Manager</h2>
 		</div>
 	</div>
 
 	<div id="container">
 		<div id="content">
+
+			<!-- Put new Button: add customer -->
+			<input type="button" value="Add customer"
+				onclick="window.location.href='showFormForAdd'; return false;"
+				class="add-button" />
+				
+			<!--  add a search box -->
+            <form:form action="search" method="POST">
+                Search customer: <input type="textbox" name="theSearchName" />               
+                <input type="submit" value="Search" class="add-button" />
+            </form:form>
+            	
 			<table>
 				<tr>
 					<th>First Name</th>
 					<th>Last Name</th>
 					<th>Email</th>
+					<th>Action</th>
 				</tr>
-				
+
 				<c:forEach var="tempCustomer" items="${customers}">
-					<tr> 
-						<td> ${tempCustomer.firstName}</td>
-						<td> ${tempCustomer.lastName}</td>
-						<td> ${tempCustomer.email}</td>
+
+					<!-- Construct an update link with customer id -->
+					<c:url var="updateLink" value="/customer/showFormForUpdate">
+						<c:param name="customerId" value="${tempCustomer.id}" />
+					</c:url>
+					<!-- Construct an delete link with customer id -->
+					<c:url var="deleteLink" value="/customer/delete">
+						<c:param name="customerId" value="${tempCustomer.id}" />
+					</c:url>
+
+					<tr>
+						<td>${tempCustomer.firstName}</td>
+						<td>${tempCustomer.lastName}</td>
+						<td>${tempCustomer.email}</td>
+	 					<td><a href="${updateLink}">Update</a> 
+	 						<a href="${deleteLink}"
+	 							onclick="if (!(confirm('Are you sure you want to delete this customer?'))) return false">| Delete </a>
+	 					</td> 
+<%-- 			or---->		<a href="${pageContext.request.contextPath}/customer/updateCustomer?customerId=${tempCustomer.id}">Update</a> --%>
 					</tr>
-					
-				
 				</c:forEach>
-				
-			
+
+
 			</table>
 		</div>
 	</div>
--- src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -7,7 +7,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.luv2code.springdemo.entity.Customer;
 
@@ -17,21 +16,91 @@ public class CustomerDAOImpl implements CustomerDAO {
 	// need to inject Session Factory
 	@Autowired
 	private SessionFactory sessionFactory;
-	
-	@Override
-//	@Transactional   --- removed, while defined in Service Layer
-	public List<Customer> getCustomers() {    // import java.util.List;
 
-		// get the current hibernate session 
-		Session currentSession= sessionFactory.getCurrentSession();  // import org.hibernate.Session;
-		
-		// create a query
-		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class); // import org.hibernate.query.Query; (hibernate spaeter als 5.2)
-		
+	@Override
+	// @Transactional --- removed, while defined in Service Layer
+	public List<Customer> getCustomers() { // import java.util.List;
+
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession(); // import
+																		// org.hibernate.Session;
+		// create a query and sort by last name
+		Query<Customer> theQuery = currentSession.createQuery("from Customer order by lastName", Customer.class);
+		// import org.hibernate.query.Query; (hibernate spaeter als 5.2)
 		// execute query and get the result list
 		List<Customer> theCustomers = theQuery.getResultList();
-		
 		// return the results
+		return theCustomers;
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession(); // import
+																		// org.hibernate.Session;
+		// save the customer
+		currentSession.saveOrUpdate(theCustomer);
+	}
+
+	@Override
+	public Customer getCustomer(int theId) {
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession(); // import
+																		// org.hibernate.Session;
+		// read or retrieve from database using the primary key
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+
+		return theCustomer;
+	}
+
+	@Override
+	public void deleteCustomer(int theId) {
+
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession(); // import
+																		// org.hibernate.Session;
+		// delete the customer from database using the primary key
+		Query<Customer> theQuery = currentSession.createQuery("delete from Customer where id=:customerId");
+		// import org.hibernate.query.Query; (hibernate spaeter als 5.2)
+		theQuery.setParameter("customerId", theId);
+		theQuery.executeUpdate();
+
+		// Alternative Lösung - Contra : 2 * DB transaction
+		// Customer customer = currentSession.get(Customer.class, theId);
+		// currentSession.delete(customer);
+
+	}
+
+	@Override
+	public List<Customer> searchCustomers(String theSearchName) {
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession(); // import
+																		// org.hibernate.Session;
+
+		// Query<Customer> theQuery = currentSession.createQuery("from Customer
+		// where firstName=:customerName");
+		// // import org.hibernate.query.Query; (hibernate spaeter als 5.2)
+		// theQuery.setParameter("customerName", theSearchName);
+
+		Query theQuery = null;
+
+		// only search by name if theSearchName is not empty
+		if (theSearchName != null && theSearchName.trim().length() > 0) {
+			// search for firstName or lastName ... case insensitive
+			theQuery = currentSession.createQuery(
+					"from Customer where lower(firstName) like :theName or lower(lastName) like :theName",
+					Customer.class);
+			theQuery.setParameter("theName", "%" + theSearchName.toLowerCase() + "%");
+
+		} else {
+			// theSearchName is empty ... so just get all customers
+			theQuery = currentSession.createQuery("from Customer", Customer.class);
+		}
+
+		// execute query and get the result list
+		List<Customer> theCustomers = theQuery.getResultList();
+
 		return theCustomers;
 	}
 
